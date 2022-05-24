25a3f7a6717de80a7681d340957bfeab1cd463a8
budougumi0617 <budougumi0617@gmail.com>
2022-05-24 23:34:50 +0900

    refactor: define run function

--- main.go
+++ main.go
@@ -1,15 +1,45 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	_ = http.ListenAndServe(
-		":18080",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminated server: %v", err)
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
-	)
+	}
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		if err := s.ListenAndServe(); err != nil &&
+			// http.ErrServerClosed は
+			// http.Server.Shutdown() が正常に終了したことを示すので異常ではない。
+			err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+	// グレースフルシャットダウンの終了を待つ。
+	return eg.Wait()
 }
--- main_test.go
+++ main_test.go
@@ -1,8 +1,41 @@
 package main
 
-import "testing"
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
 
-func TestMainFunc(t *testing.T) {
-	go main()
-	// 実行することはできるが…
+	"golang.org/x/sync/errgroup"
+)
+
+func TestRun(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		return run(ctx)
+	})
+	in := "message"
+	rsp, err := http.Get("http://localhost:18080/" + in)
+	if err != nil {
+		t.Errorf("failed to get: %+v", err)
+	}
+	defer rsp.Body.Close()
+	got, err := io.ReadAll(rsp.Body)
+	if err != nil {
+		t.Fatalf("failed to read body: %v", err)
+	}
+
+	// サーバの終了動作を検証する
+	cancel()
+	if err := eg.Wait(); err != nil {
+		t.Fatal(err)
+	}
+
+	// 戻り値を検証する
+	want := fmt.Sprintf("Hi, %s!", in)
+	if string(got) != want {
+		t.Errorf("want %q, but got %q", want, got)
+	}
 }

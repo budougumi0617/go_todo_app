db87ff0f13b65f591fabc3dc32b5ef95cde7a258
budougumi0617 <budougumi0617@gmail.com>
2022-05-26 02:12:00 +0900

    feat: able to change port number

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 
@@ -11,22 +12,33 @@ import (
 )
 
 func main() {
-	if err := run(context.Background()); err != nil {
+	if len(os.Args) != 2 {
+		log.Printf("need port number\n")
+		os.Exit(1)
+	}
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
+	if err != nil {
+		log.Fatalf("failed to listen port %s: %v", p, err)
+	}
+	if err := run(context.Background(), l); err != nil {
 		log.Printf("failed to terminated server: %v", err)
 		os.Exit(1)
 	}
 }
 
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
 	s := &http.Server{
-		Addr: ":18080",
+		// 引数で受け取ったnet.Listenerを利用するので、
+		// Addrフィールドは指定しない
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
 	eg, ctx := errgroup.WithContext(ctx)
 	eg.Go(func() error {
-		if err := s.ListenAndServe(); err != nil &&
+		// ListenAndServeメソッドではなく、Serveメソッドに変更する
+		if err := s.Serve(l); err != nil &&
 			// http.ErrServerClosed は
 			// http.Server.Shutdown() が正常に終了したことを示すので異常ではない。
 			err != http.ErrServerClosed {
--- main_test.go
+++ main_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"testing"
 
@@ -11,13 +12,20 @@ import (
 )
 
 func TestRun(t *testing.T) {
+	l, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		t.Fatalf("failed to listen port %v", err)
+	}
 	ctx, cancel := context.WithCancel(context.Background())
 	eg, ctx := errgroup.WithContext(ctx)
 	eg.Go(func() error {
-		return run(ctx)
+		return run(ctx, l)
 	})
 	in := "message"
-	rsp, err := http.Get("http://localhost:18080/" + in)
+	// どんなポート番号でリッスンしているのか確認
+	url := fmt.Sprintf("http://%s/%s", l.Addr().String(), in)
+	t.Logf("tray request to %q", url)
+	rsp, err := http.Get(url)
 	if err != nil {
 		t.Errorf("failed to get: %+v", err)
 	}
@@ -34,7 +42,7 @@ func TestRun(t *testing.T) {
 	}
 
 	// 戻り値を検証する
-	want := fmt.Sprintf("Hi, %s!", in)
+	want := fmt.Sprintf("Hello, %s!", in)
 	if string(got) != want {
 		t.Errorf("want %q, but got %q", want, got)
 	}

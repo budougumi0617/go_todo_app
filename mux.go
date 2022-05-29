889ef0d6e5183dd6cd02e83cd231b371f9b7410e
budougumi0617 <budougumi0617@gmail.com>
2022-05-29 11:37:35 +0900

    test: add test for NewMux

--- mux.go
+++ mux.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func NewMux() *http.ServeMux {
+func NewMux() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")

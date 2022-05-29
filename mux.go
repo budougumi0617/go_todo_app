d32022bf89cb7c92c0c7bbc8c7f10aedcd4b8014
budougumi0617 <budougumi0617@gmail.com>
2022-05-29 11:25:09 +0900

    feat: set header

--- mux.go
+++ mux.go
@@ -5,6 +5,7 @@ import "net/http"
 func NewMux() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		w.Write([]byte(`{"status": "ok"}`))
 	})
 	return mux

575860a22ba6dc51b252794735bec3de380ffbbc
budougumi0617 <budougumi0617@gmail.com>
2022-05-30 12:15:18 +0900

    feat: define db settings

---
+++ _tools/mysql/schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE `user`
+(
+    `id`       BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ユーザーの識別子',
+    `name`     varchar(20) NOT NULL COMMENT 'ユーザー名',
+    `password` VARCHAR(80) NOT NULL COMMENT 'パスワードハッシュ',
+    `role`     VARCHAR(80) NOT NULL COMMENT 'ロール',
+    `created`  DATETIME(6) NOT NULL COMMENT 'レコード作成日時',
+    `modified` DATETIME(6) NOT NULL COMMENT 'レコード修正日時',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `uix_name` (`name`) USING BTREE
+) Engine=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='ユーザー';
+
+CREATE TABLE `task`
+(
+    `id`       BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'タスクの識別子',
+    `user_id`  BIGINT UNSIGNED NOT NULL COMMENT 'タスクを作成したユーザの識別子',
+    `title`    VARCHAR(128) NOT NULL COMMENT 'タスクのタイトル',
+    `status`   VARCHAR(20)  NOT NULL COMMENT 'タスクの状態',
+    `created`  DATETIME(6) NOT NULL COMMENT 'レコード作成日時',
+    `modified` DATETIME(6) NOT NULL COMMENT 'レコード修正日時',
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fk_user_id`
+        FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+            ON DELETE RESTRICT ON UPDATE RESTRICT
+) Engine=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='タスク';

3123ab74e003703f064425fee27701956d28c878
budougumi0617 <budougumi0617@gmail.com>
2022-05-31 11:18:48 +0900

    refactor: fix impl

--- store/task.go
+++ store/task.go
@@ -6,9 +6,16 @@ import (
 	"github.com/budougumi0617/go_todo_app/entity"
 )
 
-func (r *Repository) AddTask(ctx context.Context, db Execer, t *entity.Task) error {
-	sql := `INSERT INTO task (title, status, created, modified) VALUES (?, ?, ?, ?, ?)`
-	result, err := db.ExecContext(ctx, sql, t.Title, t.Status, r.Clocker.Now(), r.Clocker.Now())
+func (r *Repository) AddTask(
+	ctx context.Context, db Execer, t *entity.Task,
+) error {
+	sql := `INSERT INTO task
+			(title, status, created, modified)
+	VALUES (?, ?, ?, ?, ?)`
+	result, err := db.ExecContext(
+		ctx, sql, t.Title, t.Status,
+		r.Clocker.Now(), r.Clocker.Now(),
+	)
 	if err != nil {
 		return err
 	}
@@ -20,7 +27,9 @@ func (r *Repository) AddTask(ctx context.Context, db Execer, t *entity.Task) err
 	return nil
 }
 
-func (r *Repository) ListTasks(ctx context.Context, db Queryer) (entity.Tasks, error) {
+func (r *Repository) ListTasks(
+	ctx context.Context, db Queryer,
+) (entity.Tasks, error) {
 	tasks := entity.Tasks{}
 	sql := `SELECT 
 				id, title,
--- store/task_test.go
+++ store/task_test.go
@@ -3,33 +3,21 @@ package store
 import (
 	"context"
 	"testing"
-	"time"
-
-	"github.com/jmoiron/sqlx"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/budougumi0617/go_todo_app/clock"
 	"github.com/budougumi0617/go_todo_app/entity"
 	"github.com/budougumi0617/go_todo_app/testutil"
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/jmoiron/sqlx"
 )
 
-func TestRepository_ListTasks(t *testing.T) {
-	ctx := context.Background()
-	// entity.Taskを作成する他のテストケースと混ざるとテストがフェイルする。
-	// そのため、トランザクションをはることでこのテストケースの中だけのテーブル状態にする。
-	tx, err := testutil.OpenDBForTest(t).BeginTxx(ctx, nil)
-	// このテスト毛＝すが完了したらもとに戻す
-	t.Cleanup(func() { _ = tx.Rollback() })
-	if err != nil {
-		t.Fatal(err)
-	}
+func prepareTasks(ctx context.Context, t *testing.T, con Execer) entity.Tasks {
+	t.Helper()
 	// 一度きれいにしておく
-	if _, err := tx.ExecContext(ctx, "DELETE FROM task;"); err != nil {
+	if _, err := con.ExecContext(ctx, "DELETE FROM task;"); err != nil {
 		t.Logf("failed to initialize task: %v", err)
 	}
-
 	c := clock.FixedClocker{}
 	wants := entity.Tasks{
 		{
@@ -45,12 +33,12 @@ func TestRepository_ListTasks(t *testing.T) {
 			Created: c.Now(), Modified: c.Now(),
 		},
 	}
-	result, err := tx.ExecContext(ctx,
+	result, err := con.ExecContext(ctx,
 		`INSERT INTO task (title, status, created, modified)
-    		VALUES
-    		    (?, ?, ?, ?),
-    		    (?, ?, ?, ?),
-    		    (?, ?, ?, ?);`,
+			VALUES
+			    (?, ?, ?, ?),
+			    (?, ?, ?, ?),
+			    (?, ?, ?, ?);`,
 		wants[0].Title, wants[0].Status, wants[0].Created, wants[0].Modified,
 		wants[1].Title, wants[1].Status, wants[1].Created, wants[1].Modified,
 		wants[2].Title, wants[2].Status, wants[2].Created, wants[2].Modified,
@@ -65,27 +53,38 @@ func TestRepository_ListTasks(t *testing.T) {
 	wants[0].ID = entity.TaskID(id)
 	wants[1].ID = entity.TaskID(id + 1)
 	wants[2].ID = entity.TaskID(id + 2)
-	t.Cleanup(func() {
-		if _, err := tx.ExecContext(ctx, "DELETE FROM task WHERE id IN (?, ?, ?);", wants[0].ID, wants[1].ID, wants[2].ID); err != nil {
-			t.Logf("failed to delete task: %v", err)
-		}
-	})
+	return wants
+}
+
+func TestRepository_ListTasks(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+	// entity.Taskを作成する他のテストケースと混ざるとテストがフェイルする。
+	// そのため、トランザクションをはることでこのテストケースの中だけのテーブル状態にする。
+	tx, err := testutil.OpenDBForTest(t).BeginTxx(ctx, nil)
+	// このテストケースが完了したらもとに戻す
+	t.Cleanup(func() { _ = tx.Rollback() })
+	if err != nil {
+		t.Fatal(err)
+	}
+	wants := prepareTasks(ctx, t, tx)
 
 	sut := &Repository{}
 	gots, err := sut.ListTasks(ctx, tx)
 	if err != nil {
 		t.Fatalf("unexected error: %v", err)
 	}
-	if d := cmp.Diff(gots, wants, cmpopts.EquateApproxTime(1*time.Second)); len(d) != 0 {
+	if d := cmp.Diff(gots, wants); len(d) != 0 {
 		t.Errorf("differs: (-got +want)\n%s", d)
 	}
 }
 
 func TestRepository_AddTask(t *testing.T) {
+	t.Parallel()
 	ctx := context.Background()
 
 	c := clock.FixedClocker{}
-	wantID := 20
+	var wantID int64 = 20
 	okTask := &entity.Task{
 		Title:    "ok task",
 		Status:   "todo",
@@ -93,32 +92,20 @@ func TestRepository_AddTask(t *testing.T) {
 		Modified: c.Now(),
 	}
 
-	tests := map[string]struct {
-		task    *entity.Task
-		wantErr bool
-	}{
-		"ok": {
-			task: okTask,
-		},
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatal(err)
 	}
-	for n, tt := range tests {
-		t.Run(n, func(t *testing.T) {
-			db, mock, err := sqlmock.New()
-			if err != nil {
-				t.Fatal(err)
-			}
-			t.Cleanup(func() { db.Close() })
-			mock.ExpectExec(
-				// エスケープが必要
-				`INSERT INTO task \(title, status, created, modified\) VALUES \(\?, \?, \?, \?, \?\)`,
-			).WithArgs(tt.task.Title, tt.task.Status, c.Now(), c.Now()).
-				WillReturnResult(sqlmock.NewResult(int64(wantID), 1))
+	t.Cleanup(func() { db.Close() })
+	mock.ExpectExec(
+		// エスケープが必要
+		`INSERT INTO task \(title, status, created, modified\) VALUES \(\?, \?, \?, \?, \?\)`,
+	).WithArgs(okTask.Title, okTask.Status, c.Now(), c.Now()).
+		WillReturnResult(sqlmock.NewResult(wantID, 1))
 
-			xdb := sqlx.NewDb(db, "mysql")
-			r := &Repository{Clocker: c}
-			if err := r.AddTask(ctx, xdb, tt.task); (err != nil) != tt.wantErr {
-				t.Errorf("AddTask() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
+	xdb := sqlx.NewDb(db, "mysql")
+	r := &Repository{Clocker: c}
+	if err := r.AddTask(ctx, xdb, okTask); err != nil {
+		t.Errorf("want no error, but got %v", err)
 	}
 }

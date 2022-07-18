c9eb8c1c6ee9fe8e064936b20996f511994f58cc
budougumi0617 <budougumi0617@binc.jp>
2022-07-19 01:44:15 +0900

    fix: 誌面に記載した最新のコード状態に変更

--- _chapter14/section54/main.go
+++ _chapter14/section54/main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
-	_ = http.ListenAndServe(
+	err := http.ListenAndServe(
 		":18080",
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	)
+	if err != nil {
+		fmt.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
+	}
 }

191a7804866284c8e70030ed63623ea1936d57a2
budougumi0617 <budougumi0617@binc.jp>
2022-07-19 01:51:22 +0900

    chore: ch14/sec55終了時点のコードを追加

---
+++ _chapter14/section55/main_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func TestRun(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		return run(ctx)
+	})
+	in := "message"
+	rsp, err := http.Get("http://localhost:18080/" + in)
+	if err != nil {
+		t.Errorf("failed to get: %+v", err)
+	}
+	defer rsp.Body.Close()
+	got, err := io.ReadAll(rsp.Body)
+	if err != nil {
+		t.Fatalf("failed to read body: %v", err)
+	}
+	// HTTPサーバーの戻り値を検証する
+	want := fmt.Sprintf("Hello, %s!", in)
+	if string(got) != want {
+		t.Errorf("want %q, but got %q", want, got)
+	}
+	// run関数に終了通知を送信する。
+	cancel()
+	// run関数の戻り値を検証する
+	if err := eg.Wait(); err != nil {
+		t.Fatal(err)
+	}
+}

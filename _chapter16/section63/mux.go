a143a1a77aba3009e1746b62a0c557c493018d65
budougumi0617 <budougumi0617@binc.jp>
2022-07-20 02:24:36 +0900

    :chore: chapter16のSec63と64を追加

---
+++ _chapter16/section63/mux.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func NewMux() http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		// 静的解析のエラーを回避するため明示的に戻り値を捨てている
+		_, _ = w.Write([]byte(`{"status": "ok"}`))
+	})
+	return mux
+}
